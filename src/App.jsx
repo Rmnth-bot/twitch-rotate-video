211d74ee6f695430c0d35a71a70fa41b044b93d4
Reymonth Ardiaca <reymonth.ardiaca@sudo.cl>
2025-09-13 02:53:27 -0300

    uploaded first version

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="video-chat-wrapper">
+        <div className="video">
+          <iframe
+            src="https://player.twitch.tv/?channel=quintero0&parent=localhost"
+            height="100%"
+            width="100%"
+            allowFullScreen
+            frameBorder="0"
+            title="Twitch Stream"
+          ></iframe>
+        </div>
+
+        <div className="chat">
+          <iframe
+            src="https://www.twitch.tv/embed/quintero0/chat?parent=localhost"
+            height="100%"
+            width="100%"
+            frameBorder="0"
+            title="Twitch Chat"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
